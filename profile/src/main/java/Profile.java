25e3490e7c05c8f74d8d9fe63a1c69a4b0649442
huoxudong125 <423huo@163.com>
2018-06-06 09:03:57 +0800

    ADD Maven 通过Profile替换不同环境下参数的Demo

--- profile/src/main/java/Profile.java
+++ profile/src/main/java/Profile.java
@@ -1,2 +1,20 @@
+import java.io.IOException;
+import java.util.Properties;
+
 public class Profile {
+    public static void main(String[] args) {
+        Properties prop = new Properties();
+        try {
+            //load a properties file from class path, inside static method
+            prop.load(Profile.class.getClassLoader().getResourceAsStream("config.properties"));
+
+            //get the property value and print it out
+            System.out.println(prop.getProperty("mysql.host"));
+            System.out.println(prop.getProperty("mysql.port"));
+
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
